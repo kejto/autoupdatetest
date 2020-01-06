9287cde6648b74a6d24202e6f8b7876948304f8d
Lubos Kato <lkato@techniserv-it.cz>
2020-01-06 11:46:51 +0100

    initial commit

---
+++ apps/portal/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'xplat-sample-electron-routing5-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'portal';
+}
